cab11ee783d76fd908f77612af9f4f42585849da
zya <qpzyahxf@163.com>
2021-07-25 18:38:42 +0800

    猜数字游戏

---
+++ guess game.c
@@ -0,0 +1,69 @@
+// 猜数字游戏
+// 1.电脑会生成一个随机数
+// 2.猜数字
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void menu()
+{
+   printf("****************************\n");
+   printf("****   1.play  0.exit   ****\n");
+   printf("****************************\n");
+}
+
+//RAND_MAX-32767
+void game()
+{  
+   //1.生成一个随机数
+   int ret = 0;
+   int guess = 0;
+   //拿时间戳来设置随机数的生成起始点
+   //time_t_time(time_t * timer)
+   //time_t
+   ret = rand()%100+1;//生成1-100的随机数
+   //2.猜数字
+   while(1)
+   {
+      printf("请猜数字:>");
+      scanf("%d", &guess);
+      if(guess>ret)
+      {
+         printf("猜大了\n");
+      }
+      else if(guess<ret)
+      {
+         printf("猜小了\n");
+      }
+      else
+      {
+         printf("恭喜你，猜对了\n");
+         break;
+      }
+   }
+}
+
+int main()
+{
+   int input = 0;
+   srand((unsigned int)time(NULL));
+   do
+   {
+      menu();
+      printf("请选择>:");
+      scanf("%d", &input);
+      switch(input)
+      {
+         case 1:
+            game();//猜数字游戏
+            break;
+         case 0:
+            printf("退出游戏\n");
+            break;
+         default:
+            printf("选择错误\n");
+            break;
+      }
+   } while (input);
+   getchar();
+   return 0;
+}
