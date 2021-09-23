0af79727f5ed54f0f0cfe65359d45f808ddffbb5
zya <qpzyahxf@163.com>
2021-09-23 23:36:04 +0800

    9.23学习笔记

---
+++ 9.23.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <windows.h>
+// int main()
+// {
+//     printf("您好！ \a\a\a\n");
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     printf("风\n林\n火\n山\n");
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int vx,vy;
+//     vx = 57;
+//     vy = vx+10;
+//     printf("vx的值是:%d。\n", vx);
+//     printf("vy的值是:%d。\n", vy);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int no;
+//     printf("请输入一个整数：");
+//     scanf("%d", &no);
+
+//     printf("你输入的是%d。\n", no);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int no;
+//     printf("请输入一个整数:");
+//     scanf("%d", &no);
+
+//     printf("它的10倍数是%d\n", 10 * no);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int no;
+//     printf("请输入一个整数：");
+//     scanf("%d", &no);
+
+//     printf("该整数加上10的结果是：%d\n", no + 10);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int no;
+//     printf("请输入一个整数：");
+//     scanf("%d", &no);
+
+//     printf("该整数减去10的结果是：%d\n", no - 10);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n1, n2;
+//     puts("请输入两个整数。");
+//     printf("整数1：");
+//     scanf("%d", &n1);
+//     printf("整数2：");
+//     scanf("%d", &n2);
+
+//     printf("它们的和是：%d\n", n1 + n2);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n1, n2;
+//     int wa;
+
+//     puts("请输入两个整数。");
+//     printf("整数1：");
+//     scanf("%d", &n1);
+//     printf("整数2：");
+//     scanf("%d", &n2);
+
+//     wa = n1 + n2;
+//     printf("它们的和是%d。\n", wa);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     puts("风");
+//     puts("林");
+//     puts("火");
+//     puts("山");
+//     system("pause");
+//     return 0;
+// }
+
+int main()
+{
+    int n1,n2,n3;
+    puts("请输入三个整数。");
+    printf("整数1：");
+    scanf("%d", &n1);
+    printf("整数2：");
+    scanf("%d", &n2);
+    printf("整数3：");
+    scanf("%d", &n3);
+
+    printf("它们的和是%d\n", n1+n2+n3);
+    system("pause");
+    return 0;
+
+}
